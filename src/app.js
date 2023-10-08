596b1adf43d86190bcaf6ce68d603588c0f7877e
leowader <101485176+leonardo15LD@users.noreply.github.com>
2023-10-08 17:06:54 -0500

    corrigiendo fecha

--- src/app.js
+++ src/app.js
@@ -5,35 +5,31 @@
 // - Acciones
 // - Moneda
 require("./database/db");
-const cors=require("cors")
+const cors = require("cors");
 const indicador = require("./database/indicadoresQuery");
 const express = require("express");
 const multer = require("multer");
 const excelToJson = require("convert-excel-to-json");
 const fs = require("fs-extra");
 const app = express();
-app.use(cors())
+app.use(cors());
 // origin:"http://localhost:3000",
 // credentials:true
 const port = 4000;
-function transformYearKeys(dataArray) {
-  return dataArray.map((data) => {
-    const transformedData = { ...data };
-
-    Object.keys(data).forEach((key) => {
-      if (key.match(/^\d{4}$/)) {
-        const newKey = `ano_${key}`;
-        transformedData[newKey] = transformedData[key];
-        delete transformedData[key];
-      }
+function cleanYearNames(data) {
+  const cleanedData = data.map((row) => {
+    const cleanedRow = {};
+    Object.keys(row).forEach((key) => {
+      const cleanedKey = key.replace(/[pr]/g, ""); // Eliminar letras 'p' y 'r'
+      cleanedRow[cleanedKey] = row[key];
     });
-
-    return transformedData;
+    return cleanedRow;
   });
+  return cleanedData;
 }
 app.get("/getIndicadores", async (req, res) => {
   const respuesta = await indicador.getIndicador("");
-  console.log(respuesta[2]);
+  console.log(respuesta[3].datos[0]);
   res.status(200).json(respuesta);
 });
 var upload = multer({ dest: "uploads/" });
@@ -52,20 +48,21 @@ app.post("/read", upload.single("file"), async (req, res) => {
           "*": "{{columnHeader}}",
         },
       });
-      
+
       //cambiando nombre de la propiedad año
       // const transformedData = transformYearKeys(excelData.Hoja1);
       // res.status(200).json({ Hoja1: transformedData });
       // console.log(transformedData[0].ano_2005);
       //respuesta normal
-      res.status(200).json({ excelData });
+
       //LEER : DESCOMENTAR LA PORCION DE CODIGO CORRESPONDIENTE AL ARCHIVO DEL INDICADOR A LEER,PARA GUARDARLO EN LA BD
-      //EJEMPLO: SI VAS A LEER EL ARCHIVO DONDE ESTAN LOS DATOS DEL PIB, ENTONCES SOLAMENTE DESCOMENTAR EL CODIGO QUE DICE (PIB), LO MISMO PARA LOS DEMAS INDICADORES. 
+      //EJEMPLO: SI VAS A LEER EL ARCHIVO DONDE ESTAN LOS DATOS DEL PIB, ENTONCES SOLAMENTE DESCOMENTAR EL CODIGO QUE DICE (PIB), LO MISMO PARA LOS DEMAS INDICADORES.
       //RUTA DE ARCHIVOS CON LOS DATOS : src/data
       //------------------------------PIB---------------------------------------
-      // console.log("respuesta PIB",{datos: excelData.Hoja1});
-      // const respuesta = await indicador.Guardar({datos: excelData.Hoja1});
-      // console.log("soy res PIB",respuesta);
+      const cleanedData = cleanYearNames(excelData.Hoja1);
+      console.log(cleanedData);
+      const respuesta = await indicador.Guardar({ datos: cleanedData });
+      console.log("soy res PIB", respuesta);
       //------------------------------------------------------------------------
       //------------------------------INFLACION---------------------------------
       // console.log("respuesta Inflacion",{datos: excelData.Sheet1});
@@ -82,6 +79,7 @@ app.post("/read", upload.single("file"), async (req, res) => {
       // const respuesta = await indicador.Guardar({ datos: excelData.Sheet1 });
       // console.log("soy res DEUDA", respuesta);
       //------------------------------------------------------------------------
+      res.status(200).json({ cleanedData });
       fs.remove(filePath);
     }
   } catch (error) {
